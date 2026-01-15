33eec7fd023e54c54eb4327f039cb08b43030b75
srisrija07 <tsrijaa07@gmail.com>
2026-01-15 17:52:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showContact() {
+    document.getElementById("contact").innerHTML =
+        'Email: <a href="mailto:tsrijaa07@gmail.com">tsrijaa07@gmail.com</a><br>' +
+        'GitHub: <a href="https://github.com/srisrija07" target="_blank">github.com/srisrija07</a><br>' +
+        'LinkedIn: <a href="https://www.linkedin.com/in/srija-tirumalagiri-826718306/" target="_blank">LinkedIn Profile</a><br>' +
+        'LeetCode: <a href="https://leetcode.com/u/srijaa_t/" target="_blank">LeetCode Profile</a>';
+}
